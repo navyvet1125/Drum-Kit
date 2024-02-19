e6488a1287c98ca441ce9be7893757d1db7d42cd
Evan Washington <enavy04@gmail.com>
2024-02-18 18:40:49 -0800

    simplified index.js.  Added color change functionality to buttons on screen.  Added keyboard functionality as well. '

--- index.js
+++ index.js
@@ -1,94 +1,43 @@
-// const tom = new Audio("sounds/tom-1.mp3")
+// create events for mouse up and down for instrument buttons.  Button text turns white when button is clicked on.
 document.querySelectorAll(".drum").forEach((button) => {
-    button.addEventListener("click",() => {
-        switch(button.innerHTML) {
+    button.addEventListener("mousedown",() => makeSound(button.innerHTML, true, "white"));
+    button.addEventListener("mouseup",() => makeSound(button.innerHTML, false, "#DA0463"));
+});
+// create events for when w,a,s,d,j,k, or l are pressed on keyboard.  Button text turns white when the appropriate key is pressed.  
+document.addEventListener("keydown",(event) => makeSound(event.key.toLowerCase(), true, "white"));
+document.addEventListener("keyup",(event) => makeSound(event.key.toLowerCase(), false, "#DA0463"));
+
+const makeSound = (key, keydown, color) => {
+          switch(key) {
             case "w":
-                new Audio("sounds/tom-1.mp3").play();
+                document.getElementsByClassName(key)[0].style.color = color
+                if (keydown) new Audio("sounds/tom-1.mp3").play();
                 break;
             case "a":
-                new Audio("sounds/tom-2.mp3").play();
+                document.getElementsByClassName(key)[0].style.color = color
+                if (keydown) new Audio("sounds/tom-2.mp3").play();
                 break;
             case "s":
-                new Audio("sounds/tom-3.mp3").play();
+                document.getElementsByClassName(key)[0].style.color = color
+                if (keydown) new Audio("sounds/tom-3.mp3").play();
                 break;
             case "d":
-                new Audio("sounds/tom-4.mp3").play();
+                document.getElementsByClassName(key)[0].style.color = color
+                if (keydown) new Audio("sounds/tom-4.mp3").play();
                 break;
             case "j":
-                new Audio("sounds/snare.mp3").play();
+                document.getElementsByClassName(key)[0].style.color = color
+                if (keydown) new Audio("sounds/snare.mp3").play();
                 break;
             case "k":
-                new Audio("sounds/kick-bass.mp3").play();
+                document.getElementsByClassName(key)[0].style.color = color
+                if (keydown) new Audio("sounds/kick-bass.mp3").play();
                 break;
             case "l":
-                new Audio("sounds/crash.mp3").play();
-                break;
- 
-            default: console.log(button.innerHTML)
-        }
-    });
-    });
-document.addEventListener("keydown",(event) => {
-        const key = event.key.toLowerCase() 
-        switch(key) {
-            case "w":
-                document.getElementsByClassName(key)[0].style.color = "white"
-                new Audio("sounds/tom-1.mp3").play();
-                break;
-            case "a":
-                document.getElementsByClassName(key)[0].style.color = "white"
-                new Audio("sounds/tom-2.mp3").play();
-                break;
-            case "s":
-                document.getElementsByClassName(key)[0].style.color = "white"
-                new Audio("sounds/tom-3.mp3").play();
-                break;
-            case "d":
-                document.getElementsByClassName(key)[0].style.color = "white"
-                new Audio("sounds/tom-4.mp3").play();
-                break;
-            case "j":
-                document.getElementsByClassName(key)[0].style.color = "white"
-                new Audio("sounds/snare.mp3").play();
-                break;
-            case "k":
-                document.getElementsByClassName(key)[0].style.color = "white"
-                new Audio("sounds/kick-bass.mp3").play();
-                break;
-            case "l":
-                document.getElementsByClassName(key)[0].style.color = "white"
-                new Audio("sounds/crash.mp3").play();
+                document.getElementsByClassName(key)[0].style.color = color
+                if (keydown) new Audio("sounds/crash.mp3").play();
                 break;
  
             default: 
-        }    
-});
-
-document.addEventListener("keyup",(event) =>{
-    const key = event.key.toLowerCase()
-    switch(key) {
-        case "w":
-            document.getElementsByClassName(key)[0].style.color = "#DA0463";
-            break;
-        case "a":
-            document.getElementsByClassName(key)[0].style.color = "#DA0463";
-            break;
-        case "s":
-            document.getElementsByClassName(key)[0].style.color = "#DA0463";
-            break;
-        case "d":
-            document.getElementsByClassName(key)[0].style.color = "#DA0463";
-            break;
-        case "j":
-            document.getElementsByClassName(key)[0].style.color = "#DA0463";
-            break;
-        case "k":
-            document.getElementsByClassName(key)[0].style.color = "#DA0463";
-            break;
-        case "l":
-            document.getElementsByClassName(key)[0].style.color = "#DA0463";
-            break;
-        default:
-
-    }
-});
+          }
+}
