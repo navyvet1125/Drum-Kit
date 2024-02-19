a13b7ae03ed25f4039784c92253488dfad2879a9
Evan Washington <enavy04@gmail.com>
2024-02-18 18:45:12 -0800

    clarified index.js code a little bit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // create events for mouse up and down for instrument buttons.  Button text turns white when button is clicked on.
 document.querySelectorAll(".drum").forEach((button) => {
-    button.addEventListener("mousedown",() => makeSound(button.innerHTML, true, "white"));
-    button.addEventListener("mouseup",() => makeSound(button.innerHTML, false, "#DA0463"));
+    const key = button.innerHTML;
+    button.addEventListener("mousedown",() => makeSound(key, true, "white"));
+    button.addEventListener("mouseup",() => makeSound(key, false, "#DA0463"));
 });
 // create events for when w,a,s,d,j,k, or l are pressed on keyboard.  Button text turns white when the appropriate key is pressed.  
 document.addEventListener("keydown",(event) => makeSound(event.key.toLowerCase(), true, "white"));
