d48d9039a9849928ddad0df041677daf6577abd7
Evan Washington <enavy04@gmail.com>
2024-02-18 20:02:44 -0800

    streamlined code functionality.

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-// create events for mouse up and down for instrument buttons.  Button text turns white when button is clicked on.
+// create event when user clicks on an instrument buttons.
 document.querySelectorAll(".drum").forEach((button) => {
     const key = button.innerHTML;
-    button.addEventListener("mousedown",() => makeSound(key, true, "white"));
-    button.addEventListener("mouseup",() => makeSound(key, false, "#DA0463"));
+    button.addEventListener("click",() => {
+        buttonAnimation(key)
+        makeSound(key) 
+    });
 });
-// create events for when w,a,s,d,j,k, or l are pressed on keyboard.  Button text turns white when the appropriate key is pressed.  
-document.addEventListener("keydown",(event) => makeSound(event.key.toLowerCase(), true, "white"));
-document.addEventListener("keyup",(event) => makeSound(event.key.toLowerCase(), false, "#DA0463"));
+// create events for when w,a,s,d,j,k, or l are pressed on keyboard.  
+document.addEventListener("keydown",(event) => {
+    const key = event.key.toLowerCase();
+    buttonAnimation(key);    
+    makeSound(key);
+});
 
-const makeSound = (key, keydown, color) => {
+const makeSound = (key) => {
           switch(key) {
             case "w":
-                document.getElementsByClassName(key)[0].style.color = color
-                if (keydown) new Audio("sounds/tom-1.mp3").play();
+                new Audio("sounds/tom-1.mp3").play();
                 break;
             case "a":
-                document.getElementsByClassName(key)[0].style.color = color
-                if (keydown) new Audio("sounds/tom-2.mp3").play();
+                new Audio("sounds/tom-2.mp3").play();
                 break;
             case "s":
-                document.getElementsByClassName(key)[0].style.color = color
-                if (keydown) new Audio("sounds/tom-3.mp3").play();
+                new Audio("sounds/tom-3.mp3").play();
                 break;
             case "d":
-                document.getElementsByClassName(key)[0].style.color = color
-                if (keydown) new Audio("sounds/tom-4.mp3").play();
+                new Audio("sounds/tom-4.mp3").play();
                 break;
             case "j":
-                document.getElementsByClassName(key)[0].style.color = color
-                if (keydown) new Audio("sounds/snare.mp3").play();
+                new Audio("sounds/snare.mp3").play();
                 break;
             case "k":
-                document.getElementsByClassName(key)[0].style.color = color
-                if (keydown) new Audio("sounds/kick-bass.mp3").play();
+                new Audio("sounds/kick-bass.mp3").play();
                 break;
             case "l":
-                document.getElementsByClassName(key)[0].style.color = color
-                if (keydown) new Audio("sounds/crash.mp3").play();
+                new Audio("sounds/crash.mp3").play();
                 break;
  
             default: 
           }
+}
+// Animates the specific button that triggered the event.
+const buttonAnimation = (key) => {
+    // button pressed state.
+    document.querySelector('.'+key).classList.add('pressed')
+    document.querySelector('.'+key).style.color = "white"
+    //after 1/10th of a second, the but goes back to normal. 
+    setTimeout(()=> {
+        document.querySelector('.'+key).classList.remove('pressed')
+        document.querySelector('.'+key).style.color = "#DA0463"
+    },100)
 }
