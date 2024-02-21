2c32f46f50936219218f7203b65962953789dcb7
Evan Washington <enavy04@gmail.com>
2024-02-20 23:12:44 -0800

    removed switch statement in favor of creating an object to store new Audio objects.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const soundBank ={
+  w: new Audio("sounds/tom-1.mp3"),
+  a: new Audio("sounds/tom-2.mp3"),
+  s: new Audio("sounds/tom-3.mp3"),
+  d: new Audio("sounds/tom-4.mp3"),
+  j: new Audio("sounds/snare.mp3"),
+  k: new Audio("sounds/kick-bass.mp3"),
+  l: new Audio("sounds/crash.mp3")
+}
+
 // create event when user clicks on an instrument buttons.
 $('.drum').toArray().forEach((button) => $(button).on("click",() => makeSound($(button).html())));
 
@@ -5,33 +15,10 @@ $('.drum').toArray().forEach((button) => $(button).on("click",() => makeSound($(
 $(document).on("keydown", (event) => makeSound(event.key.toLowerCase()));
 
 const makeSound = (key) => {
-          switch(key) {
-            case "w":
-                new Audio("sounds/tom-1.mp3").play();
-                break;
-            case "a":
-                new Audio("sounds/tom-2.mp3").play();
-                break;
-            case "s":
-                new Audio("sounds/tom-3.mp3").play();
-                break;
-            case "d":
-                new Audio("sounds/tom-4.mp3").play();
-                break;
-            case "j":
-                new Audio("sounds/snare.mp3").play();
-                break;
-            case "k":
-                new Audio("sounds/kick-bass.mp3").play();
-                break;
-            case "l":
-                new Audio("sounds/crash.mp3").play();
-                break;
- 
-            default:
-                return; 
-          }
+    if(soundBank[key]){
+        soundBank[key].play();
         buttonAnimation(key);    
+    }
 }
 // Animates the specific button that triggered the event.
 // Changes button to pressed state. after 1/10th of a second, the button goes back to normal. 
